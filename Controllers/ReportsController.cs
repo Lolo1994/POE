293add2a0be28bbc52cf72c751afe3303e8f9b8f
Lonwabo Mbane <Lonwabo Mbane@MBANE-PC>
2022-11-11 02:43:41 +0200

    Finished display of 'Total monetary donations received' and 'Total number of goods received'

---
+++ Controllers/ReportsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Task_2.Data;
+using Task_2.Models;
+
+namespace Task_2.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        List<Report> totalMoneyDonations = new List<Report>();
+        List<MoneyDonations> moneyDonations = new List<MoneyDonations>();
+        List<GoodsDonations> goodsDonations = new List<GoodsDonations>();
+
+        public ReportsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reports
+        public async Task<IActionResult> Index()
+        {
+            
+            moneyDonations = _context.MoneyDonations.ToList();
+            goodsDonations = _context.GoodsDonations.ToList();
+
+            Report rep;
+            double donation = 0;
+            int numGoods = 0;
+            for (int x = 0; x <= moneyDonations.Count - 1; x++)
+            {
+
+                donation += moneyDonations[x].donationAmount;
+            }
+
+            for (int y = 0; y <= goodsDonations.Count - 1; y++)
+            {
+                numGoods += goodsDonations[y].itemNumber;
+            }
+
+            rep = new Report();
+            rep.id = 1;
+            rep.totalMonetaryDonationsReceived = donation;
+            rep.totalNumberOfGoodsReceived = numGoods;
+            totalMoneyDonations.Add(rep);
+
+            return View(totalMoneyDonations);
+        }
+
+       
+    }
+}
---
+++ Data/Migrations/20221111005452_112.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Task_2.Data.Migrations
+{
+    public partial class _112 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Report",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    totalMonetaryDonationsReceived = table.Column<double>(type: "float", nullable: false),
+                    totalNumberOfGoodsReceived = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Report", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Report");
+        }
+    }
+}
