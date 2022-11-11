e95178adfb6748a6718f09f5ffb5a8a0b7a8bb65
Lonwabo Mbane <Lonwabo Mbane@MBANE-PC>
2022-11-11 03:37:16 +0200

    Finished Reports Page and created Active Disaster class and View Page

--- Controllers/ReportsController.cs
+++ Controllers/ReportsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Task_2.Data;
 using Task_2.Models;
+using System.Dynamic;
 
 namespace Task_2.Controllers
 {
@@ -16,6 +17,7 @@ namespace Task_2.Controllers
         List<Report> totalMoneyDonations = new List<Report>();
         List<MoneyDonations> moneyDonations = new List<MoneyDonations>();
         List<GoodsDonations> goodsDonations = new List<GoodsDonations>();
+        List<ActiveDisasters> activeDisasters = new List<ActiveDisasters>();
 
         public ReportsController(ApplicationDbContext context)
         {
@@ -25,8 +27,68 @@ namespace Task_2.Controllers
         // GET: Reports
         public async Task<IActionResult> Index()
         {
+
+            moneyDonations = _context.MoneyDonations.ToList();
+            goodsDonations = _context.GoodsDonations.ToList();
+            activeDisasters = _context.ActiveDisasters.ToList();
+
+          
+
+            Report rep;
+            double donation = 0;
+            int numGoods = 0;
+            for (int x = 0; x <= moneyDonations.Count - 1; x++)
+            {
+
+                donation += moneyDonations[x].donationAmount;
+            }
+
+            for (int y = 0; y <= goodsDonations.Count - 1; y++)
+            {
+                numGoods += goodsDonations[y].itemNumber;
+            }
+
+            rep = new Report();
+            rep.id = 1;
+            rep.totalMonetaryDonationsReceived = donation;
+            rep.totalNumberOfGoodsReceived = numGoods;
+            totalMoneyDonations.Add(rep);
+
+            List<GoodsDonations> gd = new List<GoodsDonations>();
+            List<MoneyDonations> md = new List<MoneyDonations>();
+
+            for (int x = 0; x <= activeDisasters.Count - 1; x++)
+            {
+                string activeD = activeDisasters[x].DisasterType;
+
+                for (int y = 0; y <= goodsDonations.Count - 1; y++)
+                {
+                    if (goodsDonations[y].disasteType.Equals(activeD))
+                    {
+                        GoodsDonations gdItem = new GoodsDonations();
+                        gdItem = goodsDonations[y];
+                        gd.Add(gdItem);
+                    }
+                }
+                for (int z = 0; z <= moneyDonations.Count - 1; z++)
+                {
+                    if (moneyDonations[z].disasteType.Equals(activeD))
+                    {
+                        MoneyDonations mdItem = new MoneyDonations();
+                        mdItem = moneyDonations[z];
+                        md.Add(mdItem);
+                    }
+                }
             
-         
+            }
+
+
+            dynamic mymodal = new ExpandoObject();
+            mymodal.Report = totalMoneyDonations;
+            mymodal.MoneyDonations = md;
+            mymodal.GoodsDonations = gd;
+
+            return View(mymodal);
         }
 
        
---
+++ Data/Migrations/20221111014827_113.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Task_2.Data.Migrations
+{
+    public partial class _113 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ActiveDisasters",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DisasterType = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActiveDisasters", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActiveDisasters");
+        }
+    }
+}
